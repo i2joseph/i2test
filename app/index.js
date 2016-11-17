b4479d4932c971ce5f86965564725428f4218f0c
Jwing28 <jonathan.wing.lee@gmail.com>
2016-11-17 15:53:26 -0800

    [add] add routes to get JSON data

--- app/index.js
+++ app/index.js
@@ -1,3 +1,23 @@
-console.log (   $.getJSON('../source/industryintel.json',function(data){
-  console.log(data);
-})       )
+$.ajax({
+  url:'http://localhost:3000/industryintel',
+  type:'GET',
+  contentType:'application/json',
+  success:function(data){
+    console.log('data',data);
+  },
+  error:function(err){
+    console.log('error ', err);
+  }
+});
+
+$.ajax({
+  url:'http://localhost:3000/table',
+  type:'GET',
+  contentType:'application/json',
+  success:function(data){
+    console.log('data',data);
+  },
+  error:function(err){
+    console.log('error ', err);
+  }
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+//both of these work correctly
+var industryintel = require('../source/industryintel.json');
+var table = require('../source/table.json');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE, HEAD");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
+
+app.get('/industryintel', function (req, res) {
+  res.send(industryintel);
+});
+
+app.get('/table', function (req, res) {
+  res.send(table);
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!'); 
 });
